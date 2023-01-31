215fc58a3a088e9ebdb0a326e6bb856c2e945c09
Adelowo Ajibola <adelowo.ajibola@suretree.com>
2023-01-31 09:49:16 +0100

    First rebuild to master

---
+++ src/definitions/chakra/theme/foundations/font.ts
@@ -0,0 +1,8 @@
+import type { DeepPartial, Theme } from "@chakra-ui/react";
+
+const fonts: DeepPartial<Theme["fonts"]> = {
+    body: "'Poppins', sans-serif",
+    heading: "'Poppins', sans-serif",
+};
+
+export default fonts;
